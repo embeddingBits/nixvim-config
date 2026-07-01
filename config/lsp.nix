894239c578b7123bd88b0820749627a6c8f96701
ebits <tspamiitesh@gmail.com>
2026-07-01 12:58:22 +0530

    Nixvim: Remove C3 LSP

--- config/lsp.nix
+++ config/lsp.nix
@@ -7,7 +7,6 @@
                         ts_ls.enable = true;
                         lua_ls.enable = true;
                         clangd.enable = true;
-                        c3_lsp.enable = true;
                         zls.enable = true;
                         gopls.enable = true;
                         ols.enable = true;
